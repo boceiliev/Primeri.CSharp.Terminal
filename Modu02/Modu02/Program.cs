38643cae7a400de342d0ced5ac9b0ed576322f04
Bojidar Iliev <boce.iliev@gmail.com>
2016-11-06 17:36:56 +0200

    Modul 02 Video 02 preomenliivi ot tip int

--- Modu02/Modu02/Program.cs
+++ Modu02/Modu02/Program.cs
@@ -7,14 +7,24 @@ namespace Modu02
 		public static void Main(string[] args)
 		{
 			//Дефиниране на променливи
-
+			int a = 0, b = 0;
+			int sum = 0;
 
 			//Събиране на числа
+			a = 55;
+			b = 10;
 
+			sum = a + b;
 
 
 			//Писане в конзолата
+			Console.Write(a);
+			Console.Write(" + ");
+			Console.Write(b);
+			Console.Write(" = ");
 
+			Console.WriteLine(sum);
+			Console.ReadKey();
 
 		}
 	}
