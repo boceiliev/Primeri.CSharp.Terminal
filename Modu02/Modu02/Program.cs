038975f75af57bded8bcf4b6f71040aa8c2abefd
Bojidar Iliev <boce.iliev@gmail.com>
2016-11-06 17:21:39 +0200

    Добавяне на коментари

---
+++ Modu02/Modu02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modu02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+
+			//Писане в конзолата
+
+
+		}
+	}
+}
